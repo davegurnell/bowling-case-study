f4bba13d69de5c8efa69b8bdce83c63ecbcf5499
Dave Gurnell <dave@cartographer.io>
2019-10-21 11:13:09 +0100

    First attempt (using Frames)

--- src/main/scala/code/Main.scala
+++ src/main/scala/code/Main.scala
@@ -3,3 +3,78 @@ package code
 object Main extends App {
   println("Hello world!")
 }
+
+case class Frame(first: Int, second: Option[Int], bonus: Option[Int] = None) {
+  def isStrike: Boolean =
+    first == 10
+
+  def isSpare: Boolean =
+    !isStrike && first + second.getOrElse(0) == 10
+
+  def score1: Int =
+    first
+
+  def score2: Int =
+    second.getOrElse(0)
+
+  def bonusScore: Int =
+    bonus.getOrElse(0)
+
+  def rolls: List[Int] =
+    List(first) ++ second.toList ++ bonus.toList
+}
+
+object Parser {
+  val Triple = "(.)(.)(.)".r
+  val Pair = "(.)(.)".r
+  val Digit = "([0-9])".r
+
+  def parseLine(str: String): List[Frame] =
+    str.split(" +").toList.map(parseFrame)
+
+  def parseFrame(str: String): Frame =
+    str match {
+      case Triple("X", "X", "X")           => Frame(10, Some(10), Some(10))
+      case Triple("X", "X", "-")           => Frame(10, Some(10), Some(0))
+      case Triple("X", "X", Digit(c))      => Frame(10, Some(10), Some(c.toInt))
+
+      case Triple("X", Digit(b), "/")      => Frame(10, Some(b.toInt), Some(10 - b.toInt))
+      case Triple("X", Digit(b), "-")      => Frame(10, Some(b.toInt), Some(0))
+      case Triple("X", Digit(b), Digit(c)) => Frame(10, Some(b.toInt), Some(c.toInt))
+
+      case Triple(Digit(a), "/", "-")      => Frame(a.toInt, Some(10 - a.toInt), Some(0))
+      case Triple(Digit(a), "/", Digit(c)) => Frame(a.toInt, Some(10 - a.toInt), Some(c.toInt))
+
+      case Pair("-", "/")                  => Frame(0, Some(10))
+      case Pair("-", "-")                  => Frame(0, Some(0))
+
+      case Pair(Digit(a), "/")             => Frame(a.toInt, Some(10 - a.toInt))
+      case Pair(Digit(a), "-")             => Frame(a.toInt, Some(0))
+      case Pair(Digit(a), Digit(b))        => Frame(a.toInt, Some(b.toInt))
+
+      case "X" => Frame(10, None)
+
+      case other => throw new Exception(s"Bad frame: $other")
+    }
+}
+
+object Scorer {
+  def scoreLine(line: List[Frame], accum: Int = 0): Int =
+    line match {
+      case Nil => accum
+      case head :: Nil => accum + scoreLastFrame(head)
+      case head :: rest => scoreLine(rest, accum + scoreFrame(head, rest.flatMap(_.rolls)))
+    }
+
+  def scoreFrame(curr: Frame, rest: List[Int]): Int =
+    if(curr.isStrike) {
+      10 + rest.take(2).sum
+    } else if(curr.isSpare) {
+      10 + rest.take(1).sum
+    } else {
+      curr.score1 + curr.score2
+    }
+
+  def scoreLastFrame(curr: Frame): Int =
+    curr.score1 + curr.score2 + curr.bonusScore
+}
--- src/test/scala/code/MainSpec.scala
+++ src/test/scala/code/MainSpec.scala
@@ -3,9 +3,141 @@ package code
 import org.scalatest._
 
 class MainSpec extends WordSpec with Matchers {
-  "true" should {
-    "equal false" in {
-      true shouldBe false
+  "Parser.parseFrame" should {
+    "parse a strike" in {
+      val actual = Parser.parseFrame("X")
+      val expected = Frame(10, None)
+      actual shouldBe expected
+    }
+
+    "parse a spare" in {
+      val actual = Parser.parseFrame("8/")
+      val expected = Frame(8, Some(2))
+      actual shouldBe expected
+    }
+
+    "parse two hits" in {
+      val actual = Parser.parseFrame("72")
+      val expected = Frame(7, Some(2))
+      actual shouldBe expected
+    }
+
+    "parse a hit and a miss" in {
+      val actual = Parser.parseFrame("7-")
+      val expected = Frame(7, Some(0))
+      actual shouldBe expected
+    }
+
+    "parse two misses" in {
+      val actual = Parser.parseFrame("--")
+      val expected = Frame(0, Some(0))
+      actual shouldBe expected
+    }
+
+    "parse a triple-stike final frame" in {
+      val actual = Parser.parseFrame("XXX")
+      val expected = Frame(10, Some(10), Some(10))
+      actual shouldBe expected
+    }
+
+    "parse a two-stike final frame" in {
+      val actual = Parser.parseFrame("XX9")
+      val expected = Frame(10, Some(10), Some(9))
+      actual shouldBe expected
+    }
+
+    "parse a spare final frame" in {
+      val actual = Parser.parseFrame("9/9")
+      val expected = Frame(9, Some(1), Some(9))
+      actual shouldBe expected
+    }
+
+    "parse a spare final frame with a miss" in {
+      val actual = Parser.parseFrame("9/-")
+      val expected = Frame(9, Some(1), Some(0))
+      actual shouldBe expected
+    }
+  }
+
+  "Parser.parseLine" should {
+    "parse a mixed line of frames" in {
+      val actual = Parser.parseLine("X 8/ 72 7- --")
+      val expected = List(
+        Frame(10, None),
+        Frame(8, Some(2)),
+        Frame(7, Some(2)),
+        Frame(7, Some(0)),
+        Frame(0, Some(0)),
+      )
+      actual shouldBe expected
+    }
+  }
+
+  "Frame.isStrike" should {
+    "return true for a strike" in {
+      Frame(10, None).isStrike shouldBe true
+    }
+
+    "return false for a spare" in {
+      Frame(9, Some(1)).isStrike shouldBe false
+    }
+
+    "return false for a regular frame" in {
+      Frame(8, Some(1)).isStrike shouldBe false
+    }
+  }
+
+  "Frame.isSpare" should {
+    "return false for a strike" in {
+      Frame(10, None).isSpare shouldBe false
+    }
+
+    "return true for a spare" in {
+      Frame(9, Some(1)).isSpare shouldBe true
+    }
+
+    "return false for a regular frame" in {
+      Frame(8, Some(1)).isSpare shouldBe false
+    }
+  }
+
+  "Scorer.scoreFrame" should {
+    "score a strike" in {
+      val actual = Scorer.scoreFrame(Frame(10, None), List(8, 2))
+      val expected = 20
+      actual shouldBe expected
+    }
+
+    "score a spare" in {
+      val actual = Scorer.scoreFrame(Frame(9, Some(1)), List(8, 2))
+      val expected = 18
+      actual shouldBe expected
+    }
+
+    "score a regular frame" in {
+      val actual = Scorer.scoreFrame(Frame(8, Some(1)), List(8, 2))
+      val expected = 9
+      actual shouldBe expected
+    }
+  }
+
+  "Scorer.scoreLine" should {
+    "score a line of regular frames" in {
+      val actual = Scorer.scoreLine(Parser.parseLine("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-"))
+      val expected = 10*9
+      actual shouldBe expected
+    }
+
+    "score a line of spares" in {
+      val actual = Scorer.scoreLine(Parser.parseLine("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5"))
+      val expected = 15*9 + 15
+      actual shouldBe expected
+    }
+
+    "score a line of strikes" in {
+      val actual = Scorer.scoreLine(Parser.parseLine("X X X X X X X X X XXX"))
+      val expected = 300
+      actual shouldBe expected
     }
   }
 }
